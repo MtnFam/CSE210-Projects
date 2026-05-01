4162d3f2fc7d5a212e8a6e1a2dc5cb16ae35752d
Liam Barber <barber.liam99@gmail.com>
2026-05-01 17:08:23 -0600

    Built a thing

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string lrb_firstName = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lrb_lastName = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.Write($"Your name is {lrb_lastName}, {lrb_firstName} {lrb_lastName}.");
     }
 }
